1dbc8d74ca740a0e8a5ef9e05e4c2676c6686d38
kiilove <jbkim@jncore.com>
2022-09-15 15:10:40 +0900

    commit on "main"

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
 import Order from "./pages/Order";
 import OrderList from "./pages/OrderList";
 
 const App = () => {
-  return (
-    <div className="App">
-      <Home />
-    </div>
-  );
+  return <div className="App">{<Home />}</div>;
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import App from "./App";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
 import Order from "./pages/Order";
 import OrderList from "./pages/OrderList";
 
 // import your route components too
-
+const isLogin = false;
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route
+        path="/"
+        element={isLogin ? <Home /> : <Navigate replace to="/login" />}
+      />
+      <Route path="/login" element={<Login />} />
       <Route path="/order" element={<Order />} />
       <Route path="/orderlist" element={<OrderList />} />
     </Routes>
